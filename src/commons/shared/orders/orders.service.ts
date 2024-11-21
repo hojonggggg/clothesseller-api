8b72fa2d848db5bc76da4c5378e9d32ee3f0063a
hojonggggg <hojonggggg@gmail.com>
2024-11-21 21:11:05 +0900

    commit

--- src/commons/shared/orders/orders.service.ts
+++ src/commons/shared/orders/orders.service.ts
@@ -267,7 +267,7 @@ export class OrdersService {
     const queryBuilder = this.wholesalerOrderRepository.createQueryBuilder("wholesalerOrder")
      .select([
        //'DATE(wholesalerOrder.createdAt) AS orderDate',
-       'SUBSTRING(wholesalerOrder.createdAt, 1, 10) AS orderDate',
+       'SUBSTRING(wholesalerOrder.createdAt, 6, 5) AS orderDate',
        'SUM(wholesalerOrder.quantityTotal) AS quantity'
      ])
      .where("DATE(wholesalerOrder.createdAt) BETWEEN :startDate AND :endDate", 
@@ -282,12 +282,13 @@ export class OrdersService {
     for (let i = 0; i < 7; i++) {
       const date = new Date(today);
       date.setDate(today.getDate() - i);
-      //const formattedDate = formatHyphenDay(date.toISOString().split('T')[0]).substring(5);
+      const formattedDate = formatHyphenDay(date.toISOString().split('T')[0]).substring(5);
+      /*
       const year = date.getFullYear();
       const month = String(date.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1
       const day = String(date.getDate()).padStart(2, '0');
       const formattedDate = `${year}-${month}-${day}`;
-      
+      */
       const existingData = results.find(r => r.orderDate === formattedDate);
       filledResults.push({
         orderDate: formattedDate,
