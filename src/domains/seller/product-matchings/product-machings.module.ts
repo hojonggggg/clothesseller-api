eecb48a117c652a20c505f98753a39d001dc84f1
hojonggggg <hojonggggg@gmail.com>
2024-12-05 17:08:15 +0900

    commit

--- src/domains/seller/product-matchings/product-machings.module.ts
+++ src/domains/seller/product-matchings/product-machings.module.ts
@@ -5,6 +5,7 @@ import { ProductsService } from 'src/commons/shared/products/products.service';
 import { ProductMatchingsService } from 'src/commons/shared/product-matchings/product-matchings.service';
 import { WholesalerProduct } from 'src/commons/shared/products/entities/wholesaler-product.entity';
 import { WholesalerProductOption } from 'src/commons/shared/products/entities/wholesaler-product-option.entity';
+import { WholesalerOrder } from 'src/commons/shared/orders/entities/wholesaler-order.entity';
 import { SellerProduct } from 'src/commons/shared/products/entities/seller-product.entity';
 import { SellerProductOption } from 'src/commons/shared/products/entities/seller-product-option.entity';
 import { SellerProductPlus } from 'src/commons/shared/products/entities/seller-product-plus.entity';
@@ -15,6 +16,7 @@ import { SellerOrder } from 'src/commons/shared/orders/entities/seller-order.ent
     TypeOrmModule.forFeature([
       WholesalerProduct, 
       WholesalerProductOption,
+      WholesalerOrder,
       SellerProduct,
       SellerProductOption,
       SellerProductPlus,
